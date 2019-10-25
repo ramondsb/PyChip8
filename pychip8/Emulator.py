e99bb705f6c2c3532b7c5438b74a075b9c777d9d
Ramon Brito <ramon.dsb@gmail.com>
2019-10-25 02:08:16 -0400

    Restructure code architecture

--- pychip8/Emulator.py
+++ pychip8/Emulator.py
@@ -8,18 +8,27 @@ class Emulator:
 
     def __init__(self):
         self.chip8 = Chip8()
-
-
-    def read_rom(filename):
-        pass
+        self.rom = ["0x00", "0xE0", "0xA1","0x23", "0x10", "0x00", "0x2B", "0xFF"]
 
 
     def run(self):
-        self.chip8.load_rom(b'\x10\x00\x2B\xFF')
+        self.chip8.load_rom(self.rom)
         self.chip8.run()
+        # self.draw_screen(self.chip8.display)
         #while(self.chip8.is_running()):
           # chip8.display_state()
           # Draw display
+          # handle_keypress()
+
+
+    def handle_keypress():
+        # TODO
+        pass
+
+
+    def draw_screen(self, screen):
+        for line in screen:
+            print(line)
 
 
 if __name__ == "__main__":
--- pychip8/chip8.py
+++ pychip8/chip8.py
@@ -1,51 +1,95 @@
-from memory import Memory
-
 class Chip8:
 
     def __init__(self):
-        self.memory = Memory()
-        self.running = False
-        self.pc = 0
+        self.memory = 4096 * [int("0x00", 16)]
+        self.pc = int("0x200", 16)
+        self.i = int("0x00", 16)
+        self.registers = 16 * [int("0x00", 16)]
+        self.delay_timer = int("0x00", 16)
+        self.sound_timer = int("0x00", 16)
+        self.keypad = 16 * [int("0x00", 16)]
+        self.display = 64 * [32 * [int("0x00", 16)]]
 
 
     def cycle(self):
-        opcode =  self.memory.read_opcode(self.pc)
-        self.decode(opcode)
-        self.pc += 2
+        opcode = self.fetch_opcode()
+
+        self.decode_opcode(opcode)
+
+        # Update timers
+        if self.delay_timer > 0:
+            self.delay_timer -= 1
+        if self.sound_timer > 0:
+            self.delay_timer -= 1
+        if self.sound_timer == 0:
+            #TODO: Integrate sound system
+            print("beep")
 
 
+    def fetch_opcode(self):
+        msb = self.memory[self.pc]
+        lsb = self.memory[self.pc + 1]
 
-    def decode(self, opcode):
-        # First group of instruction with forma x000
+        #print("msb: {}, lsb: {}".format(msb, lsb))
+
+        return ((msb << 8) | (lsb))
+
+
+    def decode_opcode(self, opcode):
+        opcode = opcode & 0xffff
+
+        def opcode_0x00XX(opcode):
+            g2 = {
+                0x00e0: self.opcode_00E0,
+                0x00ee: self.opcode_00EE
+            }
+            op = (opcode & int('0x00FF', 16))
+            g2[op](opcode)
+
         g1 = {
                 0x1000: self.sys_address,
-                0x2000: self.sys_address2
+                0x2000: self.sys_address2,
+                0xa000: self.opcode_0xAXXX,
+                0x0000: opcode_0x00XX
         }
-        op = (opcode & int.from_bytes(b'\xF0\x00', byteorder='big'))
+
+        op = (opcode & int('0xF000', 16))
+        #print("op: {}".format(hex(op)))
 
         g1[op](opcode)
 
 
     def load_rom(self, bytes):
-        self.memory.set_data(bytes)
+        for i in range(len(bytes)):
+            self.memory[i + 512] = int(bytes[i], 16)
+
+
+    def run(self):
+        self.cycle()
+        self.cycle()
 
     def sys_address(self, opcode):
         print("sys_address")
+        self.pc += 2
 
 
     def sys_address2(self, opcode):
         print("sys_address2")
 
 
-    def run(self):
-        self.running = True
-        self.cycle()
-        self.cycle()
+    def opcode_00E0(self, opcode):
+        """Clear screen"""
+        self.display = 64 * [32 * [int("0x00", 16)]]
+        self.pc += 2
+        # TODO: Set draw flag
 
 
-    def stop(self):
-        self.running= False
+    def opcode_0xAXXX(self, opcode):
+        address = opcode & 0x0FFF
+        print("address: {}".format(hex(address)))
+        self.i = address 
+        self.pc += 2
 
 
-    def is_running(self):
-        return self.running
+    def opcode_00EE(self, opcode):
+        print("Executing opcode 00EE")
