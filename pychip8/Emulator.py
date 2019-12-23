04aaacc3b3d096e6e81ac4c3cfa3a1a2776a3c72
Ramon Brito <ramon.dsb@gmail.com>
2019-12-23 13:23:55 -0400

    Use PyGame library to draw screen

--- pychip8/Emulator.py
+++ pychip8/Emulator.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import argparse
+import pygame
+import sys
 
 from chip8 import Chip8
 
+# TODO: Move to a file
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 class Emulator:
 
@@ -12,18 +17,45 @@ class Emulator:
         self.chip8 = Chip8()
         self.rom = self.load_from_file(file)
 
+        self.TITLE = "CHIP8 Emulator"
+        self.PIXEL_SIZE = 10
+        self.WIDTH = 64
+        self.HEIGHT = 32
+        self.SIZE = [self.WIDTH * self.PIXEL_SIZE, self.HEIGHT * self.PIXEL_SIZE]
+        self.MAX_TICKS_PER_SECOND = 60
+
+        pygame.init()
+
 
     def run(self):
         self.chip8.initialize()
         self.chip8.load_rom(self.rom)
-        for i in range(0, 1000):
+
+        # PyGame initialization
+        screen = pygame.display.set_mode(self.SIZE)
+        pygame.display.set_caption(self.TITLE)
+        clock = pygame.time.Clock()
+
+        # Game loop
+        while True:
+
+            # Limit the loop speed
+            clock.tick(self.MAX_TICKS_PER_SECOND)
+
+            self.handle_keypress()
+
             self.chip8.run()
-            self.chip8.draw_display()
-        # self.draw_screen(self.chip8.display)
-        #while(self.chip8.is_running()):
-          # chip8.display_state()
-          # Draw display
-          # handle_keypress()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            screen.fill(WHITE)
+
+            self.draw_screen(screen, self.chip8.display)
+
+            pygame.display.update()
 
 
     def load_from_file(self, file):
@@ -31,19 +63,34 @@ class Emulator:
             return file.read()
 
 
-    def handle_keypress():
+    def handle_keypress(self):
         # TODO
         pass
 
 
-    def draw_screen(self, screen):
-        for line in screen:
-            print(line)
+    def draw_screen(self, screen, display_state):
+
+        def draw_pixel(screen, x, y, color):
+            screen.fill(color, [
+                x * self.PIXEL_SIZE,
+                y * self.PIXEL_SIZE,
+                self.PIXEL_SIZE,
+                self.PIXEL_SIZE])
+
+        for y in range(0, self.HEIGHT):
+            for x in range(0, self.WIDTH):
+                address = x + y * self.WIDTH
+                pixel = display_state[address]
+                if pixel != 0:
+                    draw_pixel(screen, x, y, WHITE)
+                else:
+                    draw_pixel(screen, x, y, BLACK)
 
 
 def main(filepath):
     emu = Emulator(filepath)
-    emu.run();
+    emu.run()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
